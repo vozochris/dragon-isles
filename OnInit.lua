be51f669c2576d31ba08ed8c0939a75285c77562
Cristian Vozoca <vozochris@gmail.com>
2022-12-12 14:13:48 +0100

    v1.0.17

---
+++ OnInit.lua
@@ -0,0 +1,313 @@
+aura_env.dragon_isles_general = {
+    {name = "Aiding the Accord", quests = {70750}},
+    {name = "Trial of the Elements", quests = {71995}},
+    {name = "Trial of the Flood", quests = {71033}},
+    {name = "Siege on Dragonbane Keep", quests = {70866}},
+    {name = "Grand Hunt", quests = {70906}},
+    {name = "Community Feast", quests = {70893}},
+}
+
+aura_env.dragon_isles_obsidian_citadel = {
+    {name = "Allegiance to One", quests = {66419}},
+    {name = "Disarming Behaviour", quests = {65842}},
+    {name = "Shaking our Foundation", quests = {69918}},
+    {name = "Dragon Eyes", quests = {67099}},
+    {name = "Hands-Off Operation", quests = {66449}},
+    {name = "Feeding the Fire", quests = {66308}},
+    --Wrathion
+    {name = "Keys of Loyalty", quests = {66133}, required = {66802}},
+    {name = "Securing Our Legacy", quests = {72448}, required = {66802}},
+    --Sabellian
+    {name = "Keys of Loyalty", quests = {66805}, required = {66808}},
+    {name = "Greatest of Threats", quests = {72447}, required = {66808}},
+}
+
+aura_env.dragon_isles_war_mode = {
+    {name = "Supply Chest", quests = {72376}},
+}
+
+aura_env.dragon_isles_the_waking_shores = {
+    {name = "Battlehorn Pyrhus", quests = {74040}, info = "neck, trinket, gun, 1h agi dagger, 1h agi fistweap, 1h int mace, leather chest, leather shoulders, ring, warglaive, mail head, plate legs, cloth boots", coords = "29.8 58.6", super = true},
+    {name = "Cauldronbearer Blakor", quests = {74042}, info = "plate head, 1h str mace", coords = "31.4 56.6", super = true},
+    {name = "Captain Lancer", quests = {73075}, info = "polearm str, 1h str mace, plate head", coords = "27.6 76.4", super = true},
+    {name = "Char", quests = {74043}, info = "staff int, leather boots, plate shoulders, mail shoulders, mail chest, back, plate legs, trinket", coords = "30.4 51.4", super = true},
+    {name = "Rohzor Forgesmash", quests = {74052}, info = "plate head, 1h mace", coords = "27.6 60.8", super = true},
+    {name = "Turboris", quests = {74054}, info = "staff int, leather boots, plate shoulders, mail shoulders, mail chest, trinket", coords = "33.4 55.8", super = true},
+    {name = "Enkine the Voracious", quests = {73072}, info = "1h agi fistweap, 1h str axe, 1h int mace, gun, ring (crit haste), melee trinket, trinket", coords = "21.6 64.76", super = true},
+    {name = "Rasnar the War Ender", quests = {74051}, info = "plate head, 1h mace", coords = "30.22 60.43"},
+    {name = "Death's Shadow", quests = {73074}, info = "ring(crit haste), cloth feet", coords = "31.82 54.41"},
+    {name = "Shas'ith", quests = {74077}, info = "leather shoulders", coords = "24.0 57.6"},
+    {name = "Morchok", quests = {74067}, info = "neck", coords = "32.6 52.4"},
+    {name = "Worldcarver A'tir", quests = {74090}, info = "trinket", coords = "30.0 55.4"},
+    {name = "Forgotten Gryphon", quests = {72130}, info = "trinket", coords = "33.0 76.2"},
+    {name = "Qalashi War Mammoth", quests = {73890}, info = "1hand mace, 1 hand fist weap, leather shoulders, gun, ring(haste crit), plate legs, trinket", coords = "34.8 72.0"},
+    {name = "Terillod the Devout", quests = {72850}, info = "cloth hands, cloak, cloth chest, plate shoulders, cloak, mail chest, ring(crit haste), off hand, shoulders mail, warglaive, trinket", coords = "60.6 83.0"},
+    {name = "Magmaton", quests = {70979}, info = "1 hand mace", coords = "40.0 64.8"},
+    {name = "Penumbrus", quests = {70212}, info = "ring(crist haste), leather boots, 2hand staff, plate legs, trinket", coords = "27.15 60.85 -> 24.56 54.52"},
+    {name = "Shadeslash Trakken", quests = {70719}, info = "1hand sword, ring(vers mastery)", coords = "47.0 73.6"},
+    {name = "Smogswog the Firebreather", quests = {69668}, info = "mail head, trinket", coords = "69.02 64.81"},
+    {name = "Nulltheria the Void Gazer", quests = {73888}, info = "trinket int, ring(crit vers)", coords = "56.0 45.8"},
+    {name = "Anhydros the Tidetaker", quests = {73865}, info = "ring(crit haste), 2hand mace int, trinket str, 2h staff int, trinket", coords = "58.6 40.2"},
+    {name = "Gushgut the Beaksinker", quests = {70718}, info = "ring(crit haste), 2hand mace int, trinket str, 2h staff int, trinket", coords = "52.6 58.6"},
+    {name = "Klozicc the Ascended", quests = {72841}, info = "int dag, cloth belt, leather boots, plate legs, 2h staff, off hand, mail shoulders, plate shoulders, mail chest, warglaive, cloak, trinket", coords = "54.8 82.2"},
+    {name = "Skewersnout", quests = {73895}, info = "agi dagger, trinket str, neck(haste mastery)", coords = "39.62 41.24"},
+    {name = "Thunderous Matriarch", quests = {69841}, info = "mail head, cloth feet, warglaive, neck, leather chest, neck, leather shoulders, agi dagger, plate legs, trinket", coords = "45.4 35.4"},
+    {name = "Drakewing", quests = {73874}, info = "cloak, dagger agi, shoulders leather, warglaive, cloth feet", coords = "Patrols: 47.2 78.0 - 67.6 49.4"},
+    {name = "Firava the Rekindler", quests = {72839}, info = "trinket agi str, agi fistweap, 1h mace, int mace 1h, gun, ring, trinket", coords = "56.4 23.2"},
+    {name = "Ancient Hornswog", quests = {72835}, info = "shield, plate shoulders, leather chest, plate legs, leather shoulders, neck, agi dagger, warglaive, neck, cloth feet, mail head", coords = "77.6 22.4"},
+    {name = "Dragonhunter Igordan", quests = {72838}, info = "plate head, str mace 1h", coords = "64.2 33.0"},
+    {name = "Azra's Prized Peony", quests = {69839}, info = "cloth legs, shield", coords = "54.6 71.0"},
+    {name = "Primal Scythid Queen", quests = {72843}, info = "neck", coords = "81.4 37.4"},
+}
+
+aura_env.dragon_isles_ohnahran_plains = {
+    {name = "Territorial Coastling", quests = {72851}, info = "mail legs", coords = "23.6 67.0"},
+    {name = "Ripsaw the Stalker", quests = {72845}, info = "agi dagger, neck, leather chest, mail helm, leather shoulders, trinket agi, trinket", coords = "26.2 65.2"},
+    {name = "Skaara", quests = {72847}, info = "neck, dagger agi, trinket agi, mail helm, leather chest, cloth feet, warglaive, plate legs, leather shoulders, trinket", coords = "45.2 48.4"},
+    {name = "Sunscale Behemoth", quests = {72849}, info = "dagger agi, trinket agi, mail helm, plate legs, trinket", coords = "63.0 48.6"},
+    {name = "Prozela Galeshot", quests = {72815}, info = "cloth chest, cloak, off hand, mail shoulders, cloak, finger, plate shoulders, chest mail, trinket", coords = "59.8 66.8"},
+    {name = "Zenet Avis", quests = {73901}, info = "egg, cloak, ring, cloak, leather shoulders, dagger agi, trinket", coords = "31.6 64.0"},
+    {name = "Shade of Grief", quests = {69854}, info = "ring, int mace 1h, off hand, shoulder leather, cloak", coords = "29.8 41.0"},
+    {name = "Eaglemaster Niraak", quests = {74063}, info = "trinket agi int, trinket agi, leather helm, bow", coords = "50.6 66.8"},
+    {name = "Ty'foon the Ascended", quests = {72852}, info = "staff int, cloak, warglaive, cloak, off hand, mail shoulders, ring, shoulders plate, cloak, chest mail, trinket", coords = "26.0 34.2"},
+    {name = "Windseeker Avash", quests = {74088}, info = "ring, head leather, trinket agi", coords = "59.2 66.4"},
+    {name = "Blightpaw the Depraved", quests = {73869}, info = "cloth head", coords = "90.4 40.0"},
+    {name = "Enraged Sapphire", quests = {69840}, info = "plate chesst", coords = "56.6 81.4"},
+    {name = "Liskheszaera", quests = {73882}, info = "cloth shoulders, dagger agi", coords = "87.6 61.6"},
+    {name = "Scav Notail", quests = {69863}, info = "leather belt", coords = "50.0 77.6"},
+    {name = "Seeker Teryx", quests = {73894}, info = "plate wrist", coords = "61.8 13.0"},
+    {name = "Mikrin of the Raging Winds", quests = {69857}, info = "int trinket, trinket", coords = "62.8 79.8"},
+    {name = "Ronsak the Decimator", quests = {69878}, info = "leather head, trinket agi", coords = "43.6 54.4"},
+}
+
+aura_env.dragon_isles_the_azure_span = {
+    {name = "Blisterhide", quests = {73985}, info = "leather chest, plate legs, neck, agi dagger, mail head, cloth boots, leather shoulders, warglaive, trinket", coords = "14.0 30.8", super = true},
+    {name = "Gnarls", quests = {73996}, info = "shield, plate hands", coords = "14.0 37.6", super = true},
+    {name = "High Shaman Rotknuckle", quests = {74004}, info = "cloth head, leather legs, crossbow, 1h agi sword", coords = "16.2 33.6", super = true},
+    {name = "Snarglebone", quests = {74032}, info = "leather legs, crossbow", coords = "11.0 32.6", super = true},
+    {name = "Azure Pathfinder", quests = {73867}, coords = "53.2 32.8"},
+    {name = "Cascade", quests = {72836}, info = "2h sword, 2hand int mace, ring, str trink, wh staff int, trinket", coords = "23.4 32.8"},
+    {name = "Forgotten Creation", quests = {72840}, info = "neck, plate chest, 2hand polearm", coords = "38.2 59.0"},
+    {name = "Summoned Destroyer", quests = {72848}, info = "trinket str, 1hand mace int, gun, ring, fist weap, agi str trinket, trinket", coords = "70.0 33.6"},
+    {name = "Sharpfang", quests = {72846}, info = "leather pants, crossbow", coords = "36.8 32.4"},
+    {name = "Beogoka", quests = {73868}, info = "mail waist", coords = "73.2 26.4"},
+    {name = "Blue Terror", quests = {73870}, info = "trinket agi, neck, trinket", coords = "16.8 27.4"},
+    {name = "Dragonhunter Gorund", quests = {73873}, info = "agi 1h sword, plate helm", coords = "27.2 45.0"},
+    {name = "Fisherman Tinnak", quests = {72730}, coords = "49.2 38.4"},
+    {name = "Mucka the Raker", quests = {73885}, info = "leather chest, agi dagger", coords = "58.0 54.6"},
+    {name = "Mahg the Trampler", quests = {}, info = "2h mace", coords = "37.2 36.0"},
+    {name = "Spellwrought Snowman", quests = {74082}, info = "cloth helm", coords = "55.0 34.2"},
+}
+
+aura_env.dragon_isles_thaldraszus = {
+    {name = "Ancient Protector", quests = {74055}, info = "plate chest, 1h agi sword, staff int, agi polearm", coords = "59.6 58.8", super = true},
+    {name = "Henlare", quests = {72814}, info = "trinket int, warglaive, head mail, shoulders leather, feet cloth, leather chest, neck, agi dagger, plate legs, trinket", coords = "55.4 77.2", super = true},
+    {name = "Corrupted Proto-Dragon", quests = {69962}, info = "offhand int", coords = "44.8 69.0"},
+    {name = "Liskron the Dazzling", quests = {72842}, info = "shoulders leather, warglaive, leather chest, neck, agi dagger, trinket agi, plate legs feet cloth, trinket", coords = "36.6 72.8"},
+    {name = "Lookout Mordren", quests = {69967}, info = "cloak, held off hand, mace 1h int, chest mail, trinket agi str, plate shoulders, gun, warglaive, cloak", coords = "36.8 85.6"},
+    {name = "The Great Shellkhan", quests = {72121}, info = "agi trinket, warglaive", coords = "38.4 68.2"},
+    {name = "Acrosoth", quests = {72834}, info = "mail shoulders, plate armor, shield", coords = "39.8 76.4"},
+    {name = "Innumerable Ruination", quests = {}, info = "trinket int", coords = "60.6 81.4"},
+    {name = "Goremaul the Gluttonous", quests = {73878}, info = "chest mail", coords = "53.2 41.0"},
+    {name = "Pleasant Alpha", quests = {72806}, info = "leather shoulders", coords = "37.8 77.8"},
+    {name = "Eldoren the Reborn", quests = {69875}, info = "neck", coords = "49.4 50.4"},
+    {name = "Matriarch Remalla", quests = {69883}, info = "2h mace str", coords = "52.8 59.2"},
+    {name = "Phenran", quests = {69976}, info = "1h mace str", coords = "60.6 61.4"},
+    {name = "The Weeping Vilomah", quests = {74086}, info = "mail hands, plate legs", coords = "47.4 72.2"},
+    {name = "Woolfang", quests = {69850}, info = "agi dagger, leather chest", coords = "47.8 49.8"},
+}
+
+aura_env.dragon_isles_quests = {
+    {name = "Tarjin's Tales", quests = {70779}, coords = "The Waking Shores 16.0 62.6"},
+}
+
+aura_env.dragon_isles_inscription = {
+    {name = "Treatise", quests = {74105}},
+}
+
+aura_env.dragon_isles_mining = {
+    {name = "Treatise", quests = {74106}},
+}
+
+aura_env.dragon_isles_herbalism = {
+    {name = "Treatise", quests = {74107}},
+}
+
+aura_env.dragon_isles_alchemy = {
+    {name = "Treatise", quests = {74108}},
+    {name = "Master Alchemist", quests = {70247}, coords = "The Waking Shores 60.92 75.84"},
+    {name = "Frostforged Potion", quests = {70274}, coords = "The Waking Shores 55.0 81.0"},
+    {name = "Well Insulated Mug", quests = {70289}, coords = "The Waking Shores 25.1 73.3"},
+    {name = "Canteen of Suspicious Water", quests = {70305}, coords = "Ohn'ahran Plains 79.2 83.8"},
+    {name = "Experimental Decay Sample", quests = {70208}, coords = "The Azure Span:Dragon Isles 16.4 38.5"},
+    {name = "Firewater Powder Sample", quests = {70309}, coords = "The Azure Span:Dragon Isles 67.0 13.2"},
+    {name = "Furry Gloop", quests = {70278}, coords = "Thaldraszus 55.2 30.5"},
+    {name = "Contraband Concoction", quests = {70301}, coords = "Thaldraszus 59.5 38.4"},
+}
+
+aura_env.dragon_isles_blacksmithing = {
+    {name = "Treatise", quests = {74109}},
+}
+
+aura_env.dragon_isles_enchanting = {
+    {name = "Treatise", quests = {74110}},
+    {name = "Profession Master", quests = {70251}, coords = "Ohn'Ahran Plains 62.42 18.70"},
+    {name = "Flashfrozen Scroll", quests = {70320}, coords = "The Waking Shores 57.5 83.6"},
+    {name = "Lava-Infused Seed", quests = {70283}, coords = "The Waking Shores 68.0 26.8"},
+    {name = "Enchanted Debris", quests = {70272}, coords = "The Waking Shores 57.5 58.5 -> 59.86 57.10"},
+    {name = "Stormbound Horn", quests = {70291}, coords = "Ohn'ahran Plains 61.4, 67.6"},
+    {name = "Forgotten Arcane Tome", quests = {70336}, coords = "The Azure Span:Dragon Isles 38.5 59.2"},
+    {name = "Faintly Enchanted Remains", quests = {70290}, coords = "The Azure Span:Dragon Isles 45.1 61.2"},
+    {name = "Enriched Earthen Shard", quests = {70298}, coords = "The Azure Span:Dragon Isles 21.56, 45.54"},
+    {name = "Fractured Titanic Sphere", quests = {70342}, coords = "Thaldraszus 59.9 70.4"},
+}
+
+aura_env.dragon_isles_engineering = {
+    {name = "Treatise", quests = {74111}},
+    {name = "Profession Master", quests = {70252}, coords = "The Azure Span:Dragon Isles 17.8 21.7"},
+    {name = "Boomthyr Rocket", quests = {70270}, coords = "The Waking Shores 56.0 44.9"},
+    {name = "Intact Coil Capacitor", quests = {70275}, coords = "The Waking Shores 49.09 77.54"},
+}
+
+aura_env.dragon_isles_jewelcrafting = {
+    {name = "Treatise", quests = {74112}},
+    {name = "Profession Master", quests = {70255}, coords = "The Azure Span:Dragon Isles 46.23 40.84"},
+    {name = "Closely Guarded Shiny", quests = {70292}, coords = "The Waking Shores 50.4 45.1"},
+    {name = "Igneous Gem", quests = {70273}, coords = "The Waking Shores 33.9 63.7"},
+    {name = "Lofty Malygite", quests = {70282}, coords = "Ohn'ahran Plains 25.2 35.4"},
+    {name = "Fragmented Key", quests = {70263}, coords = "Ohn'ahran Plains 61.8 13.0"},
+    {name = "Crystalline Overgrowth", quests = {70277}, coords = "The Azure Span:Dragon Isles 45.0 61.3"},
+    {name = "Harmonic Crystal Harmonizer", quests = {70271}, coords = "The Azure Span:Dragon Isles 44.6 61.2"},
+    {name = "Alexstraszite Cluster", quests = {70285}, coords = "Thaldraszus 59.8 65.2"},
+    {name = "Painter's Pretty Jewel", quests = {70261}, coords = "Thaldraszus 56.91 43.72"},
+}
+
+aura_env.dragon_isles_leatherworking = {
+    {name = "Treatise", quests = {74113}},
+    {name = "Profession Master", quests = {70256}, coords = "Ohn'Ahran Plains 82.45 50.67"},
+    {name = "Poacher's Pack", quests = {70308}, coords = "The Waking Shores 39 86"},
+    {name = "Spare Djaradin Tools", quests = {70280}, coords = "The Waking Shores 64.3 25.4"},
+    {name = "Wind-Blessed Hide", quests = {70300}, coords = "Ohn'ahran Plains 86.4 53.7"},
+    {name = "Well-Danced Drum", quests = {70269}, coords = "The Azure Span:Dragon Isles 12.5 49.4"},
+    {name = "Decay-Infused Tanning Oil", quests = {70266}, coords = "The Azure Span:Dragon Isles 16.7 38.8"},
+    {name = "Treated Hides", quests = {70286}, coords = "The Azure Span:Dragon Isles 57.5 41.3"},
+    {name = "Decayed Scales", quests = {70294}, coords = "Thaldraszus 56.8 30.5"},
+}
+
+aura_env.dragon_isles_skinning = {
+    {name = "Treatise", quests = {74114}},
+}
+
+aura_env.dragon_isles_tailoring = {
+    {name = "Treatise", quests = {74115}},
+}
+
+aura_env.quest_completed = function(quest)
+    return C_QuestLog.IsQuestFlaggedCompleted(quest)
+end
+
+aura_env.add_lines = function(allStates, rares, separator, startingIndex, isNPC)
+    local index = startingIndex + 1
+    local has_rares = false
+    
+    for _, rare in ipairs(rares) do
+        local c = 0
+        local quests_count = 0
+        local show = false
+        local has_vignette = false
+        local requirement = true
+        local filtered = false
+        
+        for _, q in ipairs(aura_env.config["filter"]) do
+            if (rare.name == q.name) then
+                filtered = true
+            end
+        end
+        
+        if not filtered and rare.required then
+            for _, q in ipairs(rare.required) do
+                if not aura_env.quest_completed(q) then
+                    requirement = false
+                    break
+                end
+            end
+        end
+        
+        if not filtered and requirement then
+            for _, q in ipairs(rare.quests) do
+                quests_count = quests_count + 1
+                if not aura_env.quest_completed(q) then
+                    c = c + 1
+                end
+            end
+        end
+        
+        if c > 0 then
+            for _, vignette in pairs(aura_env.vignettes) do
+                if rare.name == vignette.name then
+                    has_vignette = true
+                    break
+                end
+            end
+            show = true
+        elseif not filtered and requirement and aura_env.config["show_completed"] then
+            show = true
+        end
+        if show then
+            local prepend = ""
+            local append = ""
+            
+            if isNPC then
+                if rare.super then
+                    prepend = WrapTextInColorCode("[S] ", "FFDDDDDD")
+                elseif aura_env.config["show_only_super_rares"] then
+                    show = false
+                end
+            end
+            
+            if show then
+                if rare.coords and aura_env.config["show_coords"] then
+                    append = append .. WrapTextInColorCode(" (" .. rare.coords .. ")", "FFDDDDDD")
+                end
+                
+                if rare.info and aura_env.config["show_info"] then
+                    append = append .. WrapTextInColorCode(" (" .. rare.info .. ")", "FFDDDDDD")
+                end
+                
+                if quests_count == 0 then
+                    append = append .. WrapTextInColorCode(" [Missing ID, defaults to completed]", "FFDDDDDD")
+                end
+                
+                if has_vignette then
+                    append = append .. " <---"
+                end
+                
+                allStates[rare] = {
+                    show = true,
+                    name = rare.name,
+                    prepend = prepend,
+                    append = append,
+                    completed = c == 0,
+                    has_vignette = has_vignette,
+                    index = index
+                }
+                index = index + 1
+                has_rares = true
+            end
+        end
+    end
+    
+    if has_rares then
+        allStates["category_" .. separator] =  {
+            show = true,
+            name = separator,
+            category = true,
+            index = startingIndex
+        }
+    end
+    
+    return index
+end
+
+aura_env.vignettes = {}
+
---
+++ Trigger1.lua
@@ -0,0 +1,90 @@
+function(allStates, event, ...)
+    if event == "VIGNETTE_MINIMAP_UPDATED" then
+        local vignetteGUID = select(1, ...)
+        local onMinimap = select(2, ...)
+        
+        if onMinimap then
+            local vignetteInfo = C_VignetteInfo.GetVignetteInfo(vignetteGUID)
+            aura_env.vignettes[vignetteGUID] = vignetteInfo
+        else
+            aura_env.vignettes[vignetteGUID] = nil
+        end
+    end
+    
+    for _, state in pairs(allStates) do
+        state.show = false
+        state.changed = true
+    end
+    
+    local index = 1
+    
+    if not aura_env.config["hide_general"] then
+        index = aura_env.add_lines(allStates, aura_env.dragon_isles_general, "General", index)
+    end
+    
+    if not aura_env.config["hide_obsidian_citadel"] then
+        index = aura_env.add_lines(allStates, aura_env.dragon_isles_obsidian_citadel, "Obsidian Citadel", index)
+    end
+    
+    if not aura_env.config["hide_war_mode"] then
+        index = aura_env.add_lines(allStates, aura_env.dragon_isles_war_mode, "War Mode", index)
+    end
+    
+    if not aura_env.config["hide_rares"] then
+        local showAllZones = aura_env.config["show_all_zones"]
+        local player_zone = C_Map.GetBestMapForUnit("player")
+        
+        if showAllZones or player_zone == 2022 then
+            index = aura_env.add_lines(allStates, aura_env.dragon_isles_the_waking_shores, "The Waking Shores", index, true)
+        end
+        if showAllZones or player_zone == 2023 then
+            index = aura_env.add_lines(allStates, aura_env.dragon_isles_ohnahran_plains, "Ohn'ahran Plains", index, true)
+        end
+        if showAllZones or player_zone == 2024 then
+            index = aura_env.add_lines(allStates, aura_env.dragon_isles_the_azure_span, "The Azure Span", index, true)
+        end
+        if showAllZones or player_zone == 2025 then
+            index = aura_env.add_lines(allStates, aura_env.dragon_isles_thaldraszus, "Thaldraszus", index, true)
+        end
+    end
+    
+    if not aura_env.config["hide_quests"] then
+        index = aura_env.add_lines(allStates, aura_env.dragon_isles_quests, "Quests", index)
+    end
+    
+    if not aura_env.config["hide_professions"] then
+        local prof1, prof2, _, _, _ = GetProfessions()
+        local name1 = ""
+        local skillLevel1 = 0
+        local name2 = ""
+        local skillLevel2 = 0
+        if (prof1) then
+            name1, _, skillLevel1 = GetProfessionInfo(prof1)
+        end
+        if (prof2) then
+            name2, _, skillLevel2 = GetProfessionInfo(prof2)
+        end
+        
+        local function addProfession(name, data)
+            local requiredSkill = 25
+            if name1 == name and skillLevel1 >= requiredSkill or name2 == name and skillLevel2 >= requiredSkill then
+                index = aura_env.add_lines(allStates, data, name, index)
+            end
+        end
+        
+        addProfession("Inscription", aura_env.dragon_isles_inscription)
+        addProfession("Mining", aura_env.dragon_isles_mining)
+        addProfession("Herbalism", aura_env.dragon_isles_herbalism)
+        addProfession("Alchemy", aura_env.dragon_isles_alchemy)
+        addProfession("Blacksmithing", aura_env.dragon_isles_blacksmithing)
+        addProfession("Enchanting", aura_env.dragon_isles_enchanting)
+        addProfession("Engineering", aura_env.dragon_isles_engineering)
+        addProfession("Jewelcrafting", aura_env.dragon_isles_jewelcrafting)
+        addProfession("Leatherworking", aura_env.dragon_isles_leatherworking)
+        addProfession("Skinning", aura_env.dragon_isles_skinning)
+        addProfession("Tailoring", aura_env.dragon_isles_tailoring)
+    end
+    
+    return true
+end
+
